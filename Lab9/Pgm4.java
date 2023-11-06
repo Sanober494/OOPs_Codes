642d36c1a66180be49bf13302bd1ccd2636cb16d
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-06 22:09:58 +0400

    Update Pgm4.java

--- Lab9/Pgm4.java
+++ Lab9/Pgm4.java
@@ -15,14 +15,11 @@ public class Pgm4 {
     public static boolean isAnagram(String str1, String str2) {
         
         str1 = str1.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        str2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-      
+        str2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();      
         char[] charArray1 = str1.toCharArray();
         char[] charArray2 = str2.toCharArray();
         Arrays.sort(charArray1);
         Arrays.sort(charArray2);
-
         return Arrays.equals(charArray1, charArray2);
     }
 }
