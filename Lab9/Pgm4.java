435fbccf37faebc3b531bea5f7218ce49ef415b1
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-11-06 22:08:59 +0400

    Create Pgm4.java

---
+++ Lab9/Pgm4.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Pgm4 {
+    public static void main(String[] args) {
+        String string1 = "parliament";
+        String string2 = "partial men";
+
+        if (isAnagram(string1, string2)) {
+            System.out.println("\"" + string1 + "\" and \"" + string2 + "\" are anagrams.");
+        } else {
+            System.out.println("\"" + string1 + "\" and \"" + string2 + "\" are not anagrams.");
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        
+        str1 = str1.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        str2 = str2.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+      
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
