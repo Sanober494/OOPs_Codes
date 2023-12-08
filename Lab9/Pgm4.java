2b176cb354644022da8bd8c374a57242e0b81e4c
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-08 21:32:00 +0400

    Update Pgm4.java

--- Lab9/Pgm4.java
+++ Lab9/Pgm4.java
@@ -1,3 +1,7 @@
+/*An anagram is a word or a phrase made by transposing the letters of another word or
+phrase; for example, "parliament" is an anagram of "partial men," and "software" is an anagram
+of "swear oft." Write a program that figures out whether one string is an anagram of another
+string. The program should ignore white space and punctuation.*/
 import java.util.Arrays;
 
 public class Pgm4 {
