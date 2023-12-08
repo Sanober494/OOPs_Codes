7b5e80f6f668981634102391b969b1f58e00c700
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-08 21:37:32 +0400

    Update Pgm3.java

--- Lab11/Pgm3.java
+++ Lab11/Pgm3.java
@@ -1,3 +1,6 @@
+/* Write a Java program to create an interface Sortable with a method sort() that sorts an array of integers in
+ascending order. Create two classes BubbleSort and SelectionSort that implement the Sortable interface and provide
+their own implementations of the sort() method.*/
 package Lab11;
 interface Sortable {
     void sort(int[] arr);
