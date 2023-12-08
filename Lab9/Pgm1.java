5bb20dc50a25476eab1cb899bdf077f7e0c21c0b
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-08 21:30:39 +0400

    Update Pgm1.java

--- Lab9/Pgm1.java
+++ Lab9/Pgm1.java
@@ -1,4 +1,16 @@
-
+/*Write a JAVA Program to construct the BITS student id (as a single String) from the
+following details read from the user. Also print the bits email id of the student.
+i) Year,
+ii) Discipline (CHEM - A1, EEE-A3, MECH-A4, CS-A7 )
+iii) Assume only PS/TS
+iv) Campus (Pilani P. Hyderabad H, Goa G, Dubai U).
+v) The 3 digit no can a non-zero random no. generated using the class Random
+Input: Year = 2014 Discipline = CS PS/TS = PS Campus = Dubai ( can be 1 of Pilani,
+Hyderabad, Goa, or Dubai)
+Output: 2014A7PS001U f2014001@dubai.bits-pilani.ac.in
+Input: Year = 2014 Discipline = MECH PS/TS = TS Campus = Pilani ( can be 1 of Pilani,
+Hyderabad, Goa, or Dubai)
+Output: 2014AAPS001P f2014001@pilani.bits-pilani.ac.in*/
 import java.util.*;
 
 public class Pgm1 {
