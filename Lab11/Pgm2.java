d0cb62601818934d0daad1439eb774f9b23675af
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-08 21:36:59 +0400

    Update Pgm2.java

--- Lab11/Pgm2.java
+++ Lab11/Pgm2.java
@@ -1,3 +1,6 @@
+/*Write a Java program to create an interface Drawable with a method draw() that takes no arguments and returns
+void. Create three classes Circle, Rectangle, and Triangle that implement the Drawable interface and override the draw()
+method to draw their respective shapes.*/
 package Lab11;
 
 interface Drawable{
