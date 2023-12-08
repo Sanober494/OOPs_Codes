2ef5b140cb496e6aee3ccbcd85537266fecbb2be
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2023-12-08 21:24:52 +0400

    Update Pgm3.java

--- Lab7/Pgm3.java
+++ Lab7/Pgm3.java
@@ -1,3 +1,10 @@
+/*Write a Java program which prints a table of a number. E.g. printTable(5) you need to
+print. Read the number n from the user.
+1 2 3 4 5 6 7 8 9 10
+2 4 6 8 10 12 14 16 18 20
+3 6 9 12 15 18 21 24 27 30
+4 8 12 16 20 24 28 32 36 40
+5 10 15 20 25 30 35 40 45 50*/
 import java.util.Scanner;
 public class Pgm3 {
     public static int printTable(int num){
